dd4994ff915540bd4e361c28f5a85eecdceb04f5
Mohamad Oghli <mhd.sh.oghli@gmail.com>
2022-08-14 15:31:26 +0400

    update get cypher table

--- app.py
+++ app.py
@@ -75,8 +75,35 @@ def caesarCipher_decode(en_msg, k):
     return decrypted_msg
 
 
+def get_cypher_table(k):
+    '''
+    Get transformation table of Caesar cypher, which is plain alphabet mapped to cypher alphabet
+    :param
+    k(int): Amount of shift position of the characters
+    :return
+    p_cypher(dict): Dictionary representing transformation table
+    '''
+    alphabet = string.ascii_uppercase
+    map_char = {}
+    p_cypher = {
+        "Plain": "Cypher"
+    }
+    k = int(k)
+    for i in range(len(alphabet)):
+        map_char[alphabet[i]] = i
+    for character in alphabet:
+        if map_char[character] + k > 25:
+            off_k = k - (25 - map_char[character]) - 1
+            p_cypher[character] = alphabet[off_k]
+        else:
+            p_cypher[character] = alphabet[map_char[character] + k]
+    return p_cypher
+
+
 if __name__ == "__main__":
     message = "Hello World!"
+    cypher_table = get_cypher_table(6)
+    print(cypher_table)
     encrypted_msg = caesarCipher_encode(message, 6)
     print(encrypted_msg)
     decrypted_msg = caesarCipher_decode(encrypted_msg, 6)
